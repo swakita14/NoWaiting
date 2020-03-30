8817126ab6fac63d38f3d28432b6138e1a645bbe
Shion Wakita <swakita14@wou.edu>
2020-03-30 15:06:55 -0700

    added context class

---
+++ NowaiterApi/DAL/NowaiterContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NowaiterApi.Models;
+
+namespace NowaiterApi.DAL
+{
+    public class NowaiterContext : DbContext
+    {
+        public NowaiterContext(DbContextOptions<NowaiterContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        
+    }
+}
