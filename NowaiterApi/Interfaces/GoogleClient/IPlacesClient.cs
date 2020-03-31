a75699a51dc5a58b3203c825ac7b264aad58fdbf
Shion Wakita <swakita14@wou.edu>
2020-03-31 13:18:17 -0700

    fixed more using issues

--- NowaiterApi/Interfaces/GoogleClient/IPlacesClient.cs
+++ NowaiterApi/Interfaces/GoogleClient/IPlacesClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GooglePlaces.Lib.Models;
 using NowaiterApi.Models.GooglePlaces;
 
 namespace NowaiterApi.Interfaces.Service
