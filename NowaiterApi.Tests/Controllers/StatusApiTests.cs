1e22003551635ec428b28cb21f5ef5b5ef5f78cd
Shion Wakita <swakita14@wou.edu>
2020-04-21 06:39:24 +0900

    added moq to package list

---
+++ NowaiterApi.Tests/Controllers/StatusApiTests.cs
@@ -0,0 +1,12 @@
+﻿using Moq;
+using NowaiterApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NowaiterApi.Tests.Controllers
+{
+    public class StatusApiTests
+    {
+    }
+}
