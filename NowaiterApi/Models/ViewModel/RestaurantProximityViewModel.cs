630b5cc7062b2f5ec227166d6718c2c5b51c34dc
Shion Wakita <swakita14@wou.edu>
2020-05-17 16:39:17 +0900

    added viewmodel for proximity search api method

---
+++ NowaiterApi/Models/ViewModel/RestaurantProximityViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NowaiterApi.Models.ViewModel
+{
+    public class RestaurantProximityViewModel
+    {
+        public int RestaurantId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address1 { get; set; }
+
+        public int? DriveThru { get; set; }
+
+        public int? InStore { get; set; }
+
+        public double DistanceTo { get; set; }
+    }
+}
